322256971e37b7d5db97270e7e4d08912c391fe4
Yuriy Ivanov <ymivanov@icloud.com>
2020-02-06 20:35:53 +0200

    implementing internationalization

--- client/src/components/navbar/NavbarComponent.js
+++ client/src/components/navbar/NavbarComponent.js
@@ -1,45 +1,65 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 const style = {
   //border: "3px solid green",
 }
-class NavbarComponent extends React.Component {
-  constructor (props) {
-    super(props); 
-  }
+const NavbarComponent = (props) => {
+  
+  const [t, i18n] = useTranslation();
 
-  render () {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-dark fixed-top" style={style} id="mainNav">
-        <div className="container">
-          <a className="navbar-brand js-scroll-trigger" href="#page-top">Hotel Premier</a>
-          <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            Menu
-            <i className="fas fa-bars"></i>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarResponsive">
-            <ul className="navbar-nav text-uppercase ml-auto">
-              <li className="nav-item">
-                <a className="nav-link js-scroll-trigger" href="#services">Services</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link js-scroll-trigger" href="#about">About</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link js-scroll-trigger" href="#team">Team</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link js-scroll-trigger" href="#contact">Contact</a>
-              </li>
-            </ul>
-          </div>
+  const changeLanguage = (e) => {
+    console.log(e);
+    //i18n.changeLanguage("uk");
+  };
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark fixed-top" style={style} id="mainNav">
+      <div className="container">
+        <a className="navbar-brand js-scroll-trigger" href="#page-top">{t("title")}</a>
+        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i className="fas fa-bars"></i>
+        </button>
+        <button className="navbar-toggler navbar-toggler-left" type="button" data-toggle="collapse" data-target="#navbarLanguage" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Language
+          <i className="fas fa-bars"></i>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarLanguage">
+          <ul className="navbar-nav mx-auto">
+            <li className="nav-item">
+              <a className="nav-link" onClick={changeLanguage} style={{cursor: "pointer"}}>UA</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" onClick={changeLanguage} style={{cursor: "pointer"}}>EN</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" onClick={changeLanguage} style={{cursor: "pointer"}}>RU</a>
+            </li>
+          </ul>
         </div>
-      </nav>
-    );
-  }
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav text-uppercase ml-auto">
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#services">Reservations</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#portfolio">Services</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#about">Our News</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#team">Contact</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#contact">About</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default NavbarComponent;
