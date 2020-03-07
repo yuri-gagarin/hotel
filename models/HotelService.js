d03dbf8d0f5a2dbe2e0da18c9993b9f3794e5b6b
Yuriy Ivanov <ymivanov@icloud.com>
2020-03-07 20:18:29 +0200

    hotel services model

---
+++ models/HotelService.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const hotelServiceSchema = new Schema({
+  serviceType: {
+    type: String,
+    required: true
+  },
+  hours: {
+    type: String,
+  },
+  price: {
+    type: Number
+  },
+  images: [{
+    type: Schema.Types.ObjectId,
+    ref: "ServiceImage"
+  }],
+  description: {
+    type: String,
+    required: true
+  },
+});
+
+export default mongoose.model("HotelService", hotelServiceSchema);
