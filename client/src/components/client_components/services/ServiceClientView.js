ebf23810aca7f66c73f11a1894f66ac1d1018ad1
Yuriy Ivanov <ymivanov@icloud.com>
2021-04-27 21:11:56 +0300

    Style additions to client ServiceDetails component

--- client/src/components/client_components/services/ServiceClientView.js
+++ client/src/components/client_components/services/ServiceClientView.js
@@ -3,13 +3,12 @@ import * as React from "react";
 // bootsrap react //
 import { Col, Image, Row, Container, Button } from "react-bootstrap";
 // additional components //
-import ImageGallery from "react-image-gallery";
-import ServiceDetails from "./ServiceDetails";
+import { AnimatedBorder } from "../shared/AnimatedBorder";
 // FLOW types //
 import type { ServiceData } from "../../../redux/reducers/service/flowTypes";
 import styles from "./css/serviceClientView.module.css";
 // helpers //
-import { setImagePath } from "../../helpers/displayHelpers";
+import { setImagePath, setStringTranslation } from "../../helpers/displayHelpers";
 
 type Props = {
   service: ServiceData,
@@ -17,6 +16,7 @@ type Props = {
 };
 
 const ServiceClientView = ({ service, triggerImgModal } : Props): React.Node => {
+  const { serviceType, price, hours, description } = service;
   const [ imgURLS, setImgURLS ] = React.useState<Array<string>>([]);
   
   React.useEffect(() => {
@@ -37,39 +37,47 @@ const ServiceClientView = ({ service, triggerImgModal } : Props): React.Node => 
 
   return (
     <React.Fragment>
-      <Row className={ styles.serviceHeaderRow }>
-        <div className={ styles.serviceTitle }>  
-          {service.serviceType}
-        </div>
-      </Row>
-      <Row className={ styles.servicesPicRow }>
-        <Col className={ styles.servicesPicCol } xs={12} md={3}>
-          <div className={ styles.serviceImgDiv } onClick={() => triggerImgModal(imgURLS, 0)}>
-            <Image className={ styles.serviceImg } src={ imgURLS[0] }></Image>
+      <AnimatedBorder animationDelay={ 500 } />
+      <Container className={ styles.serviceViewContainer }>
+        <Row className={ styles.serviceTitleRow }>
+          <div className={ styles.serviceTitle }>  
+            { serviceType ? serviceType : "Service type here..." }
           </div>
-        </Col>
-        <Col className={ styles.servicesPicCol }  xs={12} md={3}>
-          <div className={ styles.serviceImgDiv } onClick={() => triggerImgModal(imgURLS, 1)}>
-            <Image className={ styles.serviceImg } src={ imgURLS[1] }></Image>
-          </div>
-        </Col>
-        <Col className={ styles.servicesPicCol }  xs={12} md={3}>
-          <div className={ styles.serviceImgDiv } onClick={() => triggerImgModal(imgURLS, 2)}>
-            <Image className={ styles.serviceImg } src={ imgURLS[2] }></Image>
-          </div>
-        </Col>
-      </Row>
-      <Row className={ `${styles.serviceInfoRow}` }>
-        <Col className={ `${styles.serviceInfoColumn}` } xs={12} sm={12} lg={8}>
-          <ServiceDetails service={service} />
-        </Col>
-      </Row>
-      <Row className={ `${styles.serviceInfoRow}` }>
-        <Col xs={12} md={6}>
-        </Col>
-        <Col xs={12} md={6}>
-        </Col>
-      </Row>
+        </Row>
+        <Row className={ styles.servicesPicRow }>
+          <Col className={ styles.servicesPicCol } xs={12} md={3}>
+            <div className={ styles.serviceImgDiv } onClick={() => triggerImgModal(imgURLS, 0)}>
+              <Image className={ styles.serviceImg } src={ imgURLS[0] }></Image>
+            </div>
+          </Col>
+          <Col className={ styles.servicesPicCol }  xs={12} md={3}>
+            <div className={ styles.serviceImgDiv } onClick={() => triggerImgModal(imgURLS, 1)}>
+              <Image className={ styles.serviceImg } src={ imgURLS[1] }></Image>
+            </div>
+          </Col>
+          <Col className={ styles.servicesPicCol }  xs={12} md={3}>
+            <div className={ styles.serviceImgDiv } onClick={() => triggerImgModal(imgURLS, 2)}>
+              <Image className={ styles.serviceImg } src={ imgURLS[2] }></Image>
+            </div>
+          </Col>
+        </Row>
+        <Row className={ `${styles.serviceInfoRow}` }>
+          <Col className={ `${styles.serviceInfoColumn}` } xs={12} sm={12} lg={12}>
+            <div className={ styles.hoursDiv }>
+              <span>Hours</span>
+              <i className="far fa-clock"></i>
+              <span>{ hours ? hours : "No hours provided..." }</span>
+            </div>
+            {
+              price ? <div className={ styles.priceDiv }>{ price }</div>  : null
+            }
+            <div className={ styles.descDiv }>
+              <p>{ description ? setStringTranslation(description, "en") : "No description..."}</p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <AnimatedBorder animationDelay={ 500 } />
     </React.Fragment>
   )
 };
